8f5372f612bac8ec63f0527fdcbae6f4d59135b3
Zhengyang Zhao <mason.zh@gmail.com>
2023-05-15 22:23:08 -0500

    added redis connection using REDISCLOUD URL

--- redis/client.js
+++ redis/client.js
@@ -3,13 +3,19 @@ const { createClient } = require("redis");
 const { Client } = require("redis-om");
 
 module.exports.redisClient = async () => {
-    const redisClient = await new Client().open(process.env.REDIS_URL);
+    const redisClient = await new Client().open(process.env.REDISCLOUD_URL);
     return redisClient;
 };
 
 module.exports.redisNodeClient = async () => {
-    const url = process.env.REDIS_URL;
-    const connection = createClient(url);
+    // const url = process.env.REDISCLOUD_URL;
+    const connection = createClient({
+        password: process.env.REDISCLOUD_PASSWORD,
+        socket: {
+            host: process.env.REDISCLOUD_HOST,
+            port: process.env.REDISCLOUD_PORT,
+        },
+    });
     await connection.connect();
 
     return connection;
