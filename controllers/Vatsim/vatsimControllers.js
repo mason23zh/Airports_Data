8427840d163098016dec0cef7df430c40b2e03fc
Zhengyang Zhao <mason.zh@gmail.com>
2023-10-31 20:00:52 -0500

    added sortVatsimEventsTime in route

--- controllers/Vatsim/vatsimControllers.js
+++ controllers/Vatsim/vatsimControllers.js
@@ -2,16 +2,28 @@ const VatsimData = require("../../utils/Vatsim_data/VatsimData");
 module.exports.getVatsimEvents = async (req, res) => {
     const vatsimData = new VatsimData();
     const response = (await vatsimData.requestVatsimEventsData()).getAllVatsimEvents();
-    const sortedEvents = vatsimData.sortVatsimEventsByTime("end", -1);
-
-    for (let e of sortedEvents) {
-        console.log(e.id);
-        console.log(e.start_time);
-        console.log(e.end_time);
-        console.log("---");
-    }
 
     res.status(200).json({
         events: response
+    });
+};
+
+module.exports.sortVatsimEventsByTime = async (req, res) => {
+    let { target = "start" } = req.query;
+    let sort;
+    if (target !== "start" || target !== "end") {
+        target = "start";
+    }
+    if (!req.query.sort || (req.query.sort !== "-1" && req.query.sort !== "1")) {
+        sort = "1";
+    } else {
+        sort = req.query.sort;
+    }
+
+    const vatsimData = (await new VatsimData()).requestVatsimEventsData();
+    const sortedEvents = (await vatsimData).sortVatsimEventsByTime(target, Number(sort));
+
+    res.status(200).json({
+        events: sortedEvents
     });
 };
--- routes/vatsimRoutes.js
+++ routes/vatsimRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getVatsimEvents } = require("../controllers/Vatsim/vatsimControllers");
+const {
+    getVatsimEvents,
+    sortVatsimEventsByTime
+} = require("../controllers/Vatsim/vatsimControllers");
 
 router.get("/getEvents", getVatsimEvents);
+router.get("/getSortedEventsByDate", sortVatsimEventsByTime);
 
 module.exports = router;
