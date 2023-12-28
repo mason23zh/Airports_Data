0d5615b53ce618ec9f24c4535bd8a872a718ced9
Zhengyang Zhao <mason.zh@gmail.com>
2023-12-28 16:40:25 -0600

    change the vatsimHistoryTrafficsModel to fit for pre-filed data instead real-time data

--- models/vatsim/vatsimHistoryTrafficsModel.js
+++ models/vatsim/vatsimHistoryTrafficsModel.js
@@ -1,93 +1,54 @@
 const mongoose = require("mongoose");
-const vatsimHistoryTrafficsSchema = new mongoose.Schema(
-    {
-        cid: {
-            type: Number
-        },
-        name: {
+const vatsimHistoryTrafficsSchema = new mongoose.Schema({
+    cid: {
+        type: Number,
+        required: [true, "vatsim cid required"]
+    },
+    name: {
+        type: String
+    },
+    callsign: {
+        type: String
+    },
+    aircraft: {
+        full: {
             type: String
         },
-        callsign: {
+        faa: {
             type: String
         },
-        server: {
+        short: {
             type: String
-        },
-        track: [
-            {
-                latitude: {
-                    type: Number
-                },
-                longitude: {
-                    type: Number
-                },
-                altitude: {
-                    type: Number
-                },
-                groundSpeed: {
-                    type: Number
-                },
-                heading: {
-                    type: Number
-                },
-                qnhIhg: {
-                    type: Number
-                }
-            }
-        ],
-        transponder: {
-            type: String
-        },
-        flightRules: {
-            type: String
-        },
-        aircraft: {
-            full: {
-                type: String
-            },
-            faa: {
-                type: String
-            },
-            short: {
-                type: String
-            }
-        },
-        departure: {
-            type: String
-        },
-        arrival: {
-            type: String
-        },
-        alternate: {
-            type: String
-        },
-        depTime: {
-            type: String
-        },
-        enrouteTime: {
-            type: String
-        },
-        fuelTime: {
-            type: String
-        },
-        remarks: {
-            type: String
-        },
-        route: {
-            type: String
-        },
-        logonTime: {
-            type: Date
-        },
-        lastUpdated: {
-            type: Date
         }
     },
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
+    departure: {
+        type: String
+    },
+    arrival: {
+        type: String
+    },
+    alternate: {
+        type: String
+    },
+    depTime: {
+        type: String
+    },
+    enrouteTime: {
+        type: String
+    },
+    fuelTime: {
+        type: String
+    },
+    remarks: {
+        type: String
+    },
+    route: {
+        type: String
+    },
+    lastUpdated: {
+        type: Date
     }
-);
+});
 
 mongoose.plugin((vatsimHistoryTrafficsSchema) => {
     vatsimHistoryTrafficsSchema.options.toJSON = {
