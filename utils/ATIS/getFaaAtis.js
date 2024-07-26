d0b4c8319b6296efc78c28270a3cae68ad60a5bf
Zhengyang Zhao <mason.zh@gmail.com>
2024-07-25 19:03:49 -0500

    change ATIS return format

--- utils/ATIS/getFaaAtis.js
+++ utils/ATIS/getFaaAtis.js
@@ -3,9 +3,14 @@ const { FAA_ATIS_API_BASE_URL } = require("../../config");
 
 const getFaaAtis = async (location) => {
     try {
-        return await axios.get(`${FAA_ATIS_API_BASE_URL}/${location}`);
+        const response = await axios.get(`${FAA_ATIS_API_BASE_URL}/${location}`);
+        if (response.data && response.data.length > 0) {
+            return { data: response.data };
+        } else {
+            return { data: [] };
+        }
     } catch (err) {
-        return { data: "No ATIS found" };
+        return { data: [] };
     }
 };
 
