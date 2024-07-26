98ee48913d6bcf257cd7e97aae90db820f23ba77
Zhengyang Zhao <mason.zh@gmail.com>
2024-07-25 19:41:41 -0500

    added random user-agent to send request to datis.clowd.io

--- utils/ATIS/getFaaAtis.js
+++ utils/ATIS/getFaaAtis.js
@@ -1,9 +1,16 @@
 const axios = require("axios");
 const { FAA_ATIS_API_BASE_URL } = require("../../config");
+const { getRandomUserAgent } = require("../Online_Flight_Data/randomUserAgent");
 
 const getFaaAtis = async (location) => {
     try {
-        const response = await axios.get(`${FAA_ATIS_API_BASE_URL}/${location}`);
+        // const response = await axios.get(`${FAA_ATIS_API_BASE_URL}/${location}`);
+        const response = await axios.get(`${FAA_ATIS_API_BASE_URL}/${location}`, {
+            timeout: 10000,
+            headers: {
+                "User-Agent": getRandomUserAgent()
+            }
+        });
         if (response.data && response.data.length > 0) {
             return { data: response.data };
         } else {
