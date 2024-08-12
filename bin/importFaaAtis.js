67cf92f8a601f2653cec188342ffa6be9ca6a4c3
Zhengyang Zhao <mason.zh@gmail.com>
2024-08-12 16:27:42 -0500

    added importFaaAtis and importVatsimEvents to run in the heroku scheduler

---
+++ bin/importFaaAtis.js
@@ -0,0 +1,15 @@
+/*
+ * The importFaaAtis will run in the Heroku Scheduler
+ * This task will run every 60 minutes.
+ * */
+const { importFaaAtisToDB } = require("../utils/ATIS/importFaaAtisToDB");
+const logger = require("logger/index");
+
+(async () => {
+    try {
+        logger.info("Import FAA Atis to DB.");
+        await importFaaAtisToDB();
+    } catch (e) {
+        logger.error("Error occurred in scheduleJob:importFaaAtisToDB():", e);
+    }
+})();
---
+++ bin/importVatsimEvents.js
@@ -0,0 +1,14 @@
+/*
+ * The importVatsimEvents will run in the Heroku Scheduler
+ * This task will run every 60 minutes.
+ * */
+const { importVatsimEventsToDb } = require("../index");
+const logger = require("logger/index");
+
+(async () => {
+    try {
+        await importVatsimEventsToDb();
+    } catch (e) {
+        logger.error("Error occurred in scheduleJob:importFaaAtisToDB():", e);
+    }
+})();
